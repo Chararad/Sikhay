1af4d50a7b138df7e6da0d6df5fb878a61aba84a
Chararad <ralchad20067@gmail.com>
2026-05-05 21:27:50 +0800

    "menubar function on mainpage "

--- copy/Desktop/MainPage/Reconstructed/script.js
+++ copy/Desktop/MainPage/Reconstructed/script.js
@@ -1,5 +1,22 @@
 const menu = document.querySelector(".hamburger-icon-btn");
+const menubar = document.querySelector(".menu-bar");
+const blackBG = document.querySelector(".black-bg");
+
+let menuSwitch = false;
 
 menu.addEventListener("click", () =>{
-    alert("This is the menu");
-})
+
+    menuSwitch = !menuSwitch
+
+    if(menuSwitch){
+        menubar.classList.add("active");
+        menubar.classList.remove("close");
+        blackBG.classList.add("active");
+    }
+    else{
+        menubar.classList.remove("active");
+        menubar.classList.add("close");
+        blackBG.classList.remove("active");
+    }
+    
+});
